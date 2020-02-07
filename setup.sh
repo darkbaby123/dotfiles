bb10b46a91c6d932ea2aafc24a4e549a3336228f
David Chen <darkbaby123@gmail.com>
2020-02-07 17:59:12 +0800

    feat: remove hex config

--- setup.sh
+++ setup.sh
@@ -11,9 +11,6 @@ mkdir -p ~/.config/nvim
 ln -sf $PWD/nvim_init.vim ~/.config/nvim/init.vim
 ln -sf $PWD/nvim_coc_settings.json ~/.config/nvim/coc-settings.json
 
-mkdir -p ~/.hex
-ln -sf $PWD/hex_config ~/.hex/hex.config
-
 ln -sf $PWD/psqlrc ~/.psqlrc
 
 ln -sf $PWD/spacemacs ~/.spacemacs
